ba93e600dced491459d2d458be86bc14f93d6012
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-06-26 16:53:51 -0700

    Added URL for axios for testing data

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -2,7 +2,8 @@ import Vue from "vue";
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "http://127.0.0.1/vue-typescript-full-sample/",
+   // baseURL: "http://127.0.0.1/vue-typescript-full-sample/",
+   baseURL: "http://127.0.0.1/elcanter/",
    timeout: 10000
 });
 
