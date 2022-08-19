0c9a141fdf6d59409d1e33507f91fcec1d4e6606
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-08-18 22:41:50 -0700

    Now used new components created from the windows & applied some transitions into styles.scss

--- src/windows/client/ClientAddUpdate.vue
+++ src/windows/client/ClientAddUpdate.vue
@@ -8,15 +8,13 @@
                   div
 
       div.main-container(v-if="loaded" key="content")
-         div.banner
-            div.logo Inventory
-            div.text System
-         div.menu
-            div.title {{ content.title }}
-            div.subtitle {{ content.description }}
-         div.content
-            div.border-top
-            div.content-container
+         Banner
+         Menu(
+            :title="content.title"
+            :subtitle="content.description"
+         )
+         Content
+            template(#content)
                div(v-if="id > 0" class="form-group row")
                   label(class="col-sm-2 col-form-label") ID
                   div(class="col-sm-2")
@@ -142,10 +140,18 @@ import Vue from "vue";
 import mixins from "vue-typed-mixins";
 import defaultMixin from "../../plugins/mixins";
 import { Props, IPCParams, AxiosResponse, Client } from "../../interfaces/client/client-add-update";
+import Banner from "../../views/layout/Banner.vue";
+import Menu from "../../views/layout/Menu.vue";
+import Content from "../../views/layout/Content.vue";
 
 export default mixins(defaultMixin).extend({
    name: "client-add-update-component",
    mixins: [defaultMixin],
+   components: {
+      Banner,
+      Menu,
+      Content
+   },
    data() {
       return {
          id: -1,
@@ -506,87 +512,4 @@ export default mixins(defaultMixin).extend({
 
 <style lang="sass" scoped>
    @import "../../assets/scss/loader.scss"
-
-   .list-enter-active, .list-leave-active
-      transition: all 0.3s ease
-   .list-enter-from, .list-leave-to
-      opacity: 0
-
-   .main-container
-      width: 100%
-      height: 100vh
-      display: block
-      padding: 16px
-      background: #5cdb94
-      .banner
-         width: 100%
-         height: auto
-         display: block
-         background: #05386b
-         color: #edf5e1
-         padding: 40px
-         border-radius: 10px
-         display: flex
-         flex-direction: row
-         align-items: center
-         font-size: 26px
-         font-weight: bold
-         margin-bottom: 10px
-         transition: 0.3s
-         &:hover
-            box-shadow: 5px 5px #379683
-         .logo
-            background: #5cdb94
-            color: #edf5e1
-            padding: 20px
-            border-radius: 10px
-            margin-right: 20px
-            transition: 0.3s
-            &:hover
-               cursor: pointer
-               background: #05386b
-      .menu
-         width: 100%
-         height: auto
-         display: block
-         background: #edf5e1
-         border-radius: 10px
-         padding: 20px 40px
-         margin-bottom: 10px
-         transition: 0.3s
-         &:hover
-            box-shadow: 5px 5px #379683
-         .title
-            font-size: 26px
-            font-weight: bold
-            color: #edf5e1
-            border-radius: 4px
-            transition: 0.3s
-            background: linear-gradient(to left, #5cdb94 50%, #05386b 50%) right
-            background-size: 200%
-            transition: .5s ease-out
-            &:hover
-               background-position: left
-         .subtitle
-            font-size: 19px
-            color: #05386b
-      .content
-         width: 100%
-         height: auto
-         display: block
-         background: #edf5e1
-         border-radius: 10px
-         transition: 0.3s
-         &:hover
-            box-shadow: 5px 5px #379683
-         label
-            color: #05386b
-            font-weight: bold
-         .border-top
-            background: #05386b
-            height: 20px
-            border-start-start-radius: 10px
-            border-start-end-radius: 10px
-         .content-container
-            padding: 40px 36px
 </style>
--- src/windows/client/ClientDelete.vue
+++ src/windows/client/ClientDelete.vue
@@ -8,15 +8,13 @@
                   div
 
       div.main-container(v-if="loaded" key="content")
-         div.banner
-            div.logo Inventory
-            div.text System
-         div.menu
-            div.title Delete Client
-            div.subtitle The clients that will be deleted from the system
-         div.content
-            div.border-top
-            div.content-container
+         Banner
+         Menu(
+            title="Delete Client"
+            subtitle="The clients that will be deleted from the system"
+         )
+         Content
+            template(#content)
                div(v-if="id > 0" class="form-group row")
                   label(class="col-sm-2 col-form-label") ID
                   div(class="col-sm-2")
@@ -93,10 +91,18 @@ import Vue from "vue";
 import mixins from "vue-typed-mixins";
 import defaultMixin from "../../plugins/mixins";
 import { Props, IPCParams, AxiosResponse } from "../../interfaces/client/client-delete";
+import Banner from "../../views/layout/Banner.vue";
+import Menu from "../../views/layout/Menu.vue";
+import Content from "../../views/layout/Content.vue";
 
 export default mixins(defaultMixin).extend({
    name: "client-delete-component",
    mixins: [defaultMixin],
+   components: {
+      Banner,
+      Menu,
+      Content
+   },
    data() {
       return {
          id: -1,
@@ -173,87 +179,4 @@ export default mixins(defaultMixin).extend({
 
 <style lang="sass" scoped>
    @import "../../assets/scss/loader.scss"
-
-   .list-enter-active, .list-leave-active
-      transition: all 0.3s ease
-   .list-enter-from, .list-leave-to
-      opacity: 0
-
-   .main-container
-      width: 100%
-      height: 100vh
-      display: block
-      padding: 16px
-      background: #5cdb94
-      .banner
-         width: 100%
-         height: auto
-         display: block
-         background: #05386b
-         color: #edf5e1
-         padding: 40px
-         border-radius: 10px
-         display: flex
-         flex-direction: row
-         align-items: center
-         font-size: 26px
-         font-weight: bold
-         margin-bottom: 10px
-         transition: 0.3s
-         &:hover
-            box-shadow: 5px 5px #379683
-         .logo
-            background: #5cdb94
-            color: #edf5e1
-            padding: 20px
-            border-radius: 10px
-            margin-right: 20px
-            transition: 0.3s
-            &:hover
-               cursor: pointer
-               background: #05386b
-      .menu
-         width: 100%
-         height: auto
-         display: block
-         background: #edf5e1
-         border-radius: 10px
-         padding: 20px 40px
-         margin-bottom: 10px
-         transition: 0.3s
-         &:hover
-            box-shadow: 5px 5px #379683
-         .title
-            font-size: 26px
-            font-weight: bold
-            color: #edf5e1
-            border-radius: 4px
-            transition: 0.3s
-            background: linear-gradient(to left, #5cdb94 50%, #05386b 50%) right
-            background-size: 200%
-            transition: .5s ease-out
-            &:hover
-               background-position: left
-         .subtitle
-            font-size: 19px
-            color: #05386b
-      .content
-         width: 100%
-         height: auto
-         display: block
-         background: #edf5e1
-         border-radius: 10px
-         transition: 0.3s
-         &:hover
-            box-shadow: 5px 5px #379683
-         label
-            color: #05386b
-            font-weight: bold
-         .border-top
-            background: #05386b
-            height: 20px
-            border-start-start-radius: 10px
-            border-start-end-radius: 10px
-         .content-container
-            padding: 40px 36px
 </style>
