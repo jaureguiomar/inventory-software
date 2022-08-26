568563c6e2b7efdf412388f934f780f37556de7a
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-08-25 23:21:36 -0700

    Created login design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,11 @@
 <template lang="pug">
-   div
-      h4 Login
-      button(@click="something") Click me!
+   div.login
+      div.login-container
+         h1.title.mb-2 Inventory System
+         h2.subtitle.mb-4 Welcome back
+         input(class="form-control mb-2" type="text" placeholder="Username / email")
+         input(class="form-control mb-4" type="password" placeholder="Password")
+         button(class="btn btn-primary" @click="onLogin") Login
 </template>
 
 <script lang="ts">
@@ -10,7 +14,7 @@ import Vue from "vue";
 export default Vue.extend({
    name: "login-component",
    methods: {
-      something() {
+      onLogin() {
          this.$store.commit("SET_LOGGUED_IN_DATA", true);
          this.$router.push({ name: "home" });
       }
@@ -18,4 +22,29 @@ export default Vue.extend({
 });
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+   .login
+      width: 100%
+      height: 100vh
+      background: #5cdb94
+      .login-container
+         display: block
+         margin: 0 auto
+         width: 500px
+         height: auto
+         position: relative
+         top: 50%
+         transform: translateY(-50%)
+         text-align: center
+         border: 1px solid #ccc
+         padding: 60px 40px
+         background: #edf5e1
+         border-radius: 10px
+         box-shadow: 5px 5px #379683
+         color: #05386b
+         .title
+            font-size: 45px
+            font-weight: bold
+         .subtitle
+            font-size: 37px
+</style>
