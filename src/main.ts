31ebfd839ba96368e83dc2002ffaad036e961bff
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-09-20 19:38:50 -0700

    Created file for importing bbootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import i18n from "@/plugins/i18n";
 import router from "@/plugins/routes";
 import store, { key } from "@/plugins/store";
 import FontAwesomeIcon from "@/plugins/font-awesome";
+import "@/plugins/bootstrap";
 // Global Template styles
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 import "@/assets/scss/bootstrap-theme.scss";
 import "@/assets/scss/styles.scss";
 import "@/assets/scss/input-counter.scss";
