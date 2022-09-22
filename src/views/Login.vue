5b76a63d984163ed531a155c38020ac2d44b9616
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-09-22 11:59:18 -0700

    Fixed Login with quasar components and classes & added some functionalities

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,36 @@
 <template>
    <div class="login">
       <div class="login-container">
-         <h1 class="title mb-2">Inventory System</h1>
-         <h2 class="subtitle mb-4">Welcome back</h2>
-         <input class="form-control mb-2" type="text" placeholder="Username / email" />
-         <input class="form-control mb-4" type="password" placeholder="Password" />
-         <button class="btn btn-primary" @click="onLogin">Login</button>
+         <h1 class="title q-mt-none q-mb-none">Inventory System</h1>
+         <h2 class="subtitle q-mt-none q-mb-md">Welcome back</h2>
+         <q-input
+            v-model="email"
+            label="Username / Email"
+            type="text"
+            class="q-mb-md"
+            @keypress.enter="onEmailKeyup"
+         >
+         </q-input>
+         <q-input
+            v-model="password"
+            label="Password"
+            type="password"
+            class="q-mb-xl"
+            @keypress.enter="onPasswordKeyup"
+         >
+         </q-input>
+         <q-btn
+            color="primary"
+            label="Login"
+            @click="onLogin"
+         >
+         </q-btn>
       </div>
    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import { key } from "@/plugins/store";
@@ -21,7 +40,15 @@ export default defineComponent({
    setup() {
       const store = useStore(key);
       const router = useRouter();
+      const email = ref("");
+      const password = ref("");
 
+      const onEmailKeyup = () => {
+         onLogin();
+      };
+      const onPasswordKeyup = () => {
+         onLogin();
+      };
       const onLogin = () => {
          localStorage.setItem("session", JSON.stringify({
             loggued_in: true
@@ -31,6 +58,10 @@ export default defineComponent({
       }
 
       return {
+         email,
+         password,
+         onEmailKeyup,
+         onPasswordKeyup,
          onLogin
       }
    }
@@ -60,6 +91,12 @@ export default defineComponent({
          .title
             font-size: 45px
             font-weight: bold
+            h1
+               margin: 0
+               padding: 0
          .subtitle
             font-size: 37px
+            h2
+               margin: 0
+               padding: 0
 </style>
