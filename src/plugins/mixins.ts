c5f752064522314c1058ddfe13e05f81f3b1ae8e
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-09-23 19:59:16 -0700

    Removed some comments

--- src/plugins/mixins.ts
+++ src/plugins/mixins.ts
@@ -108,27 +108,3 @@ export const validateField = (value:string, newValidation:Function|any) => {
 //    else
 //       this[dataKey][field].error.message = "";
 // };
-
-// export const validateField = (value:string, field:string, dataKey:string, newValidation:Function|any) => {
-//    let is_error = false;
-
-//    if(value === "") {
-//       this[dataKey][field].error.is_error = true;
-//       this[dataKey][field].error.message = "This field is required";
-//       is_error = true;
-//    } else {
-//       this[dataKey][field].error.is_error = false;
-//       this[dataKey][field].error.message = "";
-//    }
-
-//    if(!is_error && typeof(newValidation) == "function") {
-//       const message_error = newValidation();
-//       if(message_error) {
-//          this[dataKey][field].error.is_error = true;
-//          this[dataKey][field].error.message = message_error;
-//          is_error = true;
-//       }
-//    }
-
-//    return is_error;
-// };
