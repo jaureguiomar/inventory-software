fc1d0c721479463f02d52ea3bb80a1f900e33110
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-09-26 12:46:53 -0700

    Setup store for products

--- src/plugins/store.ts
+++ src/plugins/store.ts
@@ -4,8 +4,24 @@ import { createStore, Store } from "vuex";
 interface GenericInterface {
    loaded_reply: boolean;
 }
+interface Product {
+   id: number;
+   is_active: number;
+   created: Date;
+   updated: Date;
+   code: string;
+   name: string;
+   description: string|null;
+   buy_price: string;
+   sale_price: string;
+   quantity: number;
+}
+interface SaleInterface {
+   product: Array<Product>;
+}
 interface State {
    loggued_in: boolean;
+   sale: SaleInterface;
    client: GenericInterface;
    supplier: GenericInterface;
    product: GenericInterface;
@@ -17,6 +33,9 @@ const store = createStore<State>({
    state() {
       return {
          loggued_in: false,
+         sale: {
+            product: []
+         },
          client: {
             loaded_reply: false
          },
@@ -35,6 +54,9 @@ const store = createStore<State>({
       getLogguedIn(state) {
          return state.loggued_in;
       },
+      getSaleProductReply(state) {
+         return state.sale.product;
+      },
       getClientLoadedReply(state) {
          return state.client.loaded_reply;
       },
@@ -51,6 +73,12 @@ const store = createStore<State>({
    mutations: {
       SET_LOGGUED_IN_DATA: (state, data) => {
          state.loggued_in = data;
+      },
+      SET_SALE_PRODUCT_REPLY: (state, data:Array<Product>) => {
+         state.sale.product = data;
+      },
+      ADD_SALE_PRODUCT_REPLY: (state, data:Product) => {
+         state.sale.product.push(data);
       },
       SET_CLIENT_LOADED_REPLY: (state, data:boolean) => {
          state.client.loaded_reply = data;
