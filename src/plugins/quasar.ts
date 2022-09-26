622dfa184d4fada85b9910262177f40317ecb7d4
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-09-26 13:38:55 -0700

    A fix

--- src/plugins/quasar.ts
+++ src/plugins/quasar.ts
@@ -1,8 +1,8 @@
-import "@/assets/scss/quasar.sass"
+import { Dialog } from "quasar";
 import iconSet from "quasar/icon-set/fontawesome-v5.js"
+import "@/assets/scss/quasar.sass"
 import "@quasar/extras/roboto-font/roboto-font.css"
 import "@quasar/extras/fontawesome-v5/fontawesome-v5.css"
-import { Dialog } from "quasar";
 
 export default {
    config: {},
