13a046f0bf3bbb96f9bfb9cac25e99169cc011bf
Omar Misael Torres Jauregui <omarmisael.1997@gmail.com>
2022-10-26 20:53:18 -0700

    Added user role permission atributes, retrieved data from user-role-permissions and more updates

---
+++ src/interfaces/user-role-permission/user-role-permission.ts
@@ -0,0 +1,101 @@
+import { InputFieldString } from "@/interfaces/general";
+
+// Main
+interface MainUserRolePermission {
+   id: number;
+   is_active: number;
+   name: string;
+   shortname: string;
+   description: string;
+   attr_value: string;
+}
+interface UserRolePermission extends MainUserRolePermission {
+   created: string;
+   updated: string;
+}
+interface UserRolePermissionMySQL extends MainUserRolePermission {
+   is_sync: number;
+   sync_type: "add"|"update"|"delete";
+   created: Date;
+   updated: Date;
+}
+
+// Props
+interface UserRolePermissionField {
+   name: InputFieldString;
+   shortname: InputFieldString;
+   description: InputFieldString;
+   attr_value: InputFieldString;
+}
+
+// IPCParams
+interface IPCParams {
+   id: number;
+   type: string;
+   data: UserRolePermission;
+}
+
+// IPCParams Content
+interface Content {
+   title: string;
+   description: string;
+}
+interface IPCParamsContent {
+   id: number;
+   type: string;
+   content: Content;
+   data?: UserRolePermission;
+}
+
+// Page
+interface Page {
+   id: number;
+   type: string;
+   content: Content;
+}
+
+// Window response
+interface WindowResponse {
+   id: number;
+   data: UserRolePermission|null;
+   result: string;
+   type: string;
+}
+
+// Axios response
+interface ResponseOk {
+   data: UserRolePermission;
+   ok: boolean;
+   message: string;
+}
+interface ResponseError {
+   is_error: boolean;
+   message: string|null;
+   no_error: number;
+}
+interface UserRolePermissionResponse {
+   data: ResponseOk;
+   error: ResponseError;
+}
+interface UserRolePermissionOneResponse {
+   data: UserRolePermission;
+   error: ResponseError;
+}
+interface UserRolePermissionsResponse {
+   data: Array<UserRolePermission>;
+   error: ResponseError;
+}
+
+export {
+   UserRolePermission,
+   UserRolePermissionMySQL,
+   Content,
+   IPCParams,
+   IPCParamsContent,
+   UserRolePermissionResponse,
+   UserRolePermissionOneResponse,
+   UserRolePermissionsResponse,
+   WindowResponse,
+   Page,
+   UserRolePermissionField
+};
--- src/windows/user-role/UserRoleSee.vue
+++ src/windows/user-role/UserRoleSee.vue
@@ -78,16 +78,16 @@
                         <q-separator inset></q-separator>
 
                         <q-card-section>
-                           <q-checkbox label="Permission 1" disabled></q-checkbox>
-                           <q-checkbox label="Permission 2" disabled></q-checkbox>
-                           <q-checkbox label="Permission 3" disabled></q-checkbox>
-                           <q-checkbox label="Permission 4" disabled></q-checkbox>
-                           <q-checkbox label="Permission 5" disabled></q-checkbox>
-                           <q-checkbox label="Permission 6" disabled></q-checkbox>
-                           <q-checkbox label="Permission 7" disabled></q-checkbox>
-                           <q-checkbox label="Permission 8" disabled></q-checkbox>
-                           <q-checkbox label="Permission 9" disabled></q-checkbox>
-                           <q-checkbox label="Permission 10" disabled></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 1" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 2" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 3" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 4" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 5" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 6" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 7" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 8" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 9" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 10" disable></q-checkbox>
                         </q-card-section>
                      </q-card>
                   </div>
@@ -100,16 +100,16 @@
                         <q-separator inset></q-separator>
 
                         <q-card-section>
-                           <q-checkbox label="Permission 11" disabled></q-checkbox>
-                           <q-checkbox label="Permission 12" disabled></q-checkbox>
-                           <q-checkbox label="Permission 13" disabled></q-checkbox>
-                           <q-checkbox label="Permission 14" disabled></q-checkbox>
-                           <q-checkbox label="Permission 15" disabled></q-checkbox>
-                           <q-checkbox label="Permission 16" disabled></q-checkbox>
-                           <q-checkbox label="Permission 17" disabled></q-checkbox>
-                           <q-checkbox label="Permission 18" disabled></q-checkbox>
-                           <q-checkbox label="Permission 19" disabled></q-checkbox>
-                           <q-checkbox label="Permission 20" disabled></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 11" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 12" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 13" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 14" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 15" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 16" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 17" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 18" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 19" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 20" disable></q-checkbox>
                         </q-card-section>
                      </q-card>
                   </div>
@@ -122,16 +122,16 @@
                         <q-separator inset></q-separator>
 
                         <q-card-section>
-                           <q-checkbox label="Permission 21" disabled></q-checkbox>
-                           <q-checkbox label="Permission 22" disabled></q-checkbox>
-                           <q-checkbox label="Permission 23" disabled></q-checkbox>
-                           <q-checkbox label="Permission 24" disabled></q-checkbox>
-                           <q-checkbox label="Permission 25" disabled></q-checkbox>
-                           <q-checkbox label="Permission 26" disabled></q-checkbox>
-                           <q-checkbox label="Permission 27" disabled></q-checkbox>
-                           <q-checkbox label="Permission 28" disabled></q-checkbox>
-                           <q-checkbox label="Permission 29" disabled></q-checkbox>
-                           <q-checkbox label="Permission 30" disabled></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 21" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 22" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 23" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 24" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 25" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 26" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 27" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 28" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 29" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 30" disable></q-checkbox>
                         </q-card-section>
                      </q-card>
                   </div>
@@ -144,16 +144,16 @@
                         <q-separator inset></q-separator>
 
                         <q-card-section>
-                           <q-checkbox label="Permission 31" disabled></q-checkbox>
-                           <q-checkbox label="Permission 32" disabled></q-checkbox>
-                           <q-checkbox label="Permission 33" disabled></q-checkbox>
-                           <q-checkbox label="Permission 34" disabled></q-checkbox>
-                           <q-checkbox label="Permission 35" disabled></q-checkbox>
-                           <q-checkbox label="Permission 36" disabled></q-checkbox>
-                           <q-checkbox label="Permission 37" disabled></q-checkbox>
-                           <q-checkbox label="Permission 38" disabled></q-checkbox>
-                           <q-checkbox label="Permission 39" disabled></q-checkbox>
-                           <q-checkbox label="Permission 40" disabled></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 31" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 32" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 33" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 34" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 35" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 36" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 37" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 38" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 39" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 40" disable></q-checkbox>
                         </q-card-section>
                      </q-card>
                   </div>
@@ -166,16 +166,16 @@
                         <q-separator inset></q-separator>
 
                         <q-card-section>
-                           <q-checkbox label="Permission 41" disabled></q-checkbox>
-                           <q-checkbox label="Permission 42" disabled></q-checkbox>
-                           <q-checkbox label="Permission 43" disabled></q-checkbox>
-                           <q-checkbox label="Permission 44" disabled></q-checkbox>
-                           <q-checkbox label="Permission 45" disabled></q-checkbox>
-                           <q-checkbox label="Permission 46" disabled></q-checkbox>
-                           <q-checkbox label="Permission 47" disabled></q-checkbox>
-                           <q-checkbox label="Permission 48" disabled></q-checkbox>
-                           <q-checkbox label="Permission 49" disabled></q-checkbox>
-                           <q-checkbox label="Permission 50" disabled></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 41" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 42" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 43" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 44" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 45" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 46" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 47" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 48" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 49" disable></q-checkbox>
+                           <q-checkbox v-model="checkbox" label="Permission 50" disable></q-checkbox>
                         </q-card-section>
                      </q-card>
                   </div>
@@ -195,10 +195,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from "vue";
+import { defineComponent, reactive, ref, computed, onMounted } from "vue";
 import { useI18n } from "vue-i18n/index";
+import { useStore } from "vuex";
+import Swal from "sweetalert2";
+import axios from "axios";
+import { key } from "@/plugins/store";
 import { getFormattedDateString } from "@/plugins/mixins/general";
 import { UserRole, IPCParams } from "@/interfaces/user-role/user-role";
+import { UserRolePermission, UserRolePermissionsResponse } from "@/interfaces/user-role-permission/user-role-permission";
 import Banner from "@/views/layout/Banner.vue";
 import Menu from "@/views/layout/Menu.vue";
 import Content from "@/views/layout/Content.vue";
@@ -214,6 +219,7 @@ export default defineComponent({
    },
    setup() {
       const { t } = useI18n();
+      const store = useStore(key);
       const userRole = reactive<UserRole>({
          id: -1,
          is_active: -1,
@@ -267,7 +273,13 @@ export default defineComponent({
             address: ""
          }
       });
+      const user_role_permission = ref<UserRolePermission[]>([]);
       const loaded = ref(false);
+      const checkbox = ref(true);
+
+      const getServer = computed(() => {
+         return store.getters["getServer"];
+      });
 
       window.api.receive("user-role-module-window-reply", (data:IPCParams) => {
          userRole.id = data.id;
@@ -291,6 +303,49 @@ export default defineComponent({
          loaded.value = true;
       });
 
+      onMounted(() => {
+         axios.get<UserRolePermissionsResponse>(`${ getServer.value }/user_role_permission/v3/select-all.php`)
+         .then((response) => {
+            if(response) {
+               if(!response.data.error.is_error) {
+                  const data = response.data.data;
+                  let formatted_user_role_permissions:Array<UserRolePermission> = [];
+                  for(let i = 0; i < data.length; i++) {
+                     formatted_user_role_permissions.push({
+                        id: Number(data[i].id),
+                        is_active: Number(data[i].is_active),
+                        created: data[i].created,
+                        updated: data[i].updated,
+                        name: data[i].name,
+                        shortname: data[i].shortname,
+                        description: data[i].description,
+                        attr_value: data[i].attr_value
+                     });
+                  }
+                  user_role_permission.value = formatted_user_role_permissions;
+               } else {
+                  Swal.fire({
+                     title: "Error",
+                     text: t("global.default_error"),
+                     icon: "error"
+                  });
+               }
+            } else {
+               Swal.fire({
+                  title: "Error",
+                  text: t("global.default_error"),
+                  icon: "error"
+               });
+            }
+         }).catch(() => {
+            Swal.fire({
+               title: "Error",
+               text: t("global.default_error"),
+               icon: "error"
+            });
+         });
+      });
+
       const onClose = () => {
          window.api.send("user-role-module-window-close", {
             id: userRole.id,
@@ -304,6 +359,7 @@ export default defineComponent({
          t,
          userRole,
          loaded,
+         checkbox,
          onClose
       };
    }
